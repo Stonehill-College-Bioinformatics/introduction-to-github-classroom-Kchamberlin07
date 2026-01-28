04c94ff6b29e203555f2d85bccc10cd50c21769d
Kchamberlin07 <kchamberlin1@students.stonehill.edu>
2026-01-28 18:18:49 -0500

    lab 2 update 2
    working on lab 2

--- lab2-git/Lab2.R
+++ lab2-git/Lab2.R
@@ -1,1 +1,9 @@
-#This is a test
+# ----- Loading and Exploring the iris dataset -----
+
+# Load the dataset -- note that this is a built-in dataset in R
+data(iris)
+
+# View the first few rows to understand its structure
+head(iris)
+# Check the detailed structure of the dataset for more information on its columns
+str(iris)
